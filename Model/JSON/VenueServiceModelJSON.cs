281f3fccaa55a8f720137927a3967d3ef143be91
FridaHJuul <43672790+FridaHJuul@users.noreply.github.com>
2018-12-05 11:27:16 +0100

    Add files via upload

---
+++ Model/JSON/VenueServiceModelJSON.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VenueServiceRequester.Model.JSON
+{
+   public class EquipmentCollection
+   {
+      public List<Equipment> equipments { get; set; }
+   }
+
+   public class Equipment
+   {
+      public int EquipmentId { get; set; }
+      public string EquipmentName { get; set; }
+      public string Description { get; set; }
+      public string FactoryName { get; set; }
+      public int EquipmentCount { get; set; }
+      public string BelongsToConcertHall { get; set; }
+   }
+
+
+   public class ConcerthallCollection
+   {
+      public List<Concerthall> concerthalls { get; set; }
+   }
+
+   public class Concerthall
+   {
+      public int ConcertHallId { get; set; }
+      public string ConcertHallName { get; set; }
+      public string IsLocatatVenue { get; set; }
+      public string HallResponsible { get; set; }
+      public float HallsizeM2 { get; set; }
+      public float StagesizeM2 { get; set; }
+      public int MaxNoAudience { get; set; }
+      public int NoOfDoors { get; set; }
+      public List<Equipment> Equipment { get; set; }
+   }
+
+}
---
+++ Util/JSON/VenueServiceUtilJSON.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DALRESTfulUtil.HttpClientJson;
+using VenueServiceRequester.Model.JSON;
+using VenueServiceRequester.Model.XML;
+
+namespace VenueServiceRequester.Util.JSON
+{
+   public class VenueServiceUtilJSON
+   {
+      private string _portnumber, _hostname, _servicepath;
+      private string _fullservicepath;
+
+      public VenueServiceUtilJSON(string hostname, string portno, string servicepath)
+      {
+         _portnumber = portno;
+         if (portno.Equals(""))
+            _hostname = "http://" + hostname + "/";
+         else _hostname = "http://" + hostname + ":" + portno + "/";
+         _servicepath = servicepath + "/";
+         _fullservicepath = _hostname + _servicepath;
+      }
+
+      public EquipmentCollection GetEquipment()
+      {
+         EquipmentCollection ec = new EquipmentCollection();
+         APIGetJSONAsync<List<Equipment>> getEquipment = new APIGetJSONAsync<List<Equipment>>(this._fullservicepath+"ConcertEquipments");
+         ec.equipments = getEquipment.data;
+         return ec;
+      }
+
+      public Concerthall PostConcerthall(Concerthall concerthall)
+      {
+         APIPostJSON<Concerthall> postConcerthall = new APIPostJSON<Concerthall>(this._hostname,this._servicepath+ "ConcertHalls/", concerthall);
+         return postConcerthall.data;
+      }
+
+      public Equipment PutEquipment(Equipment equipment)
+      {
+         APIPutJSON<Equipment> putEquipment = new APIPutJSON<Equipment>(this._hostname,this._servicepath + "ConcertEquipments/" + equipment.EquipmentId,equipment);
+         return putEquipment.data;
+      }
+   }
+}
